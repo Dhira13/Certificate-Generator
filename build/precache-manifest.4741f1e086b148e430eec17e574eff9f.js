0460e760e790ac9d3cedcb9b755d3d8cd29be454
Dhira13 <dhiraj.dk372@gmail.com>
2022-09-24 15:49:34 +0530

    running project

---
+++ build/precache-manifest.4741f1e086b148e430eec17e574eff9f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68c31046efa88f5b82061b542076a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55516ad8fe6532a1d31e",
+    "url": "/static/css/main.674dd84b.chunk.css"
+  },
+  {
+    "revision": "4133676de4bb415cd5f2",
+    "url": "/static/js/2.2285bd79.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/2.2285bd79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81395db462d73c7d6506",
+    "url": "/static/js/3.09767a5f.chunk.js"
+  },
+  {
+    "revision": "c23f81330ea45dc1b190d0c0df7d1b98",
+    "url": "/static/js/3.09767a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77345fc1ca067f3b94b9",
+    "url": "/static/js/4.baa2ea57.chunk.js"
+  },
+  {
+    "revision": "53730aaa15c5977de9f21ca72b9a16a5",
+    "url": "/static/js/4.baa2ea57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55516ad8fe6532a1d31e",
+    "url": "/static/js/main.388203df.chunk.js"
+  },
+  {
+    "revision": "f49e88e191a44088ad9e",
+    "url": "/static/js/runtime-main.a2af4269.js"
+  }
+]);
---
+++ src/components/DataFiles/Image.js
@@ -0,0 +1,60 @@
+const certImages = [
+    {
+        id:1,
+        src:"images/certificate-1.jpg",
+        alt:"certificte-1"
+
+    },
+    {
+        id:2,
+        src:"images/certificate-2.png",
+        alt:"certificte-1"
+
+    },
+    {
+        id:3,
+        src:"images/certificate-3.png",
+        alt:"certificte-1"
+
+    },
+    {
+        id:4,
+        src:"images/certificate-4.jpg",
+        alt:"certificte-1"
+
+    },
+    {
+        id:5,
+        src:"images/certificate-5.jpg",
+        alt:"certificte-1"
+
+    },
+    {
+        id:6,
+        src:"images/certificate-6.jpg",
+        alt:"certificte-1"
+
+    },
+    {
+        id:7,
+        src:"images/certificate-7.jpg",
+        alt:"certificte-1"
+
+    },
+    {
+        id:8,
+        src:"images/certificate-8.jpg",
+        alt:"certificte-1"
+
+    },
+    {
+        id:9,
+        src:"images/certificate-9.png",
+        alt:"certificte-1"
+
+    }
+];
+
+export default certImages;
+
+
